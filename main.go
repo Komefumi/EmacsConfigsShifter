ae2264eb82c2a442a357bdf4c64d5a5f2766e34a
Komefumi <arjunraju.pillai.1997@gmail.com>
2021-01-05 17:06:22 +0530

    Give flag usage

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
 var (
-	rootInfo *os.FileInfo
+	rootInfo     *os.FileInfo
 	rootNotExist bool
-	err error
-	files []string
+	err          error
+	files        []string
 )
 
 func mkroot(rootPath string) error {
@@ -20,12 +21,12 @@ func mkroot(rootPath string) error {
 func getRootInfo(rootPath string) (os.FileInfo, error) {
 	rootPathInfo, errRootPath := os.Stat(rootPath)
 	if errRootPath != nil {
-			if os.IsNotExist(err) {
-					fmt.Println("Root does not exist.")
-					fmt.Println("Creating root")
-			}
+		if os.IsNotExist(err) {
+			fmt.Println("Root does not exist.")
+			fmt.Println("Creating root")
+		}
 
-			return rootPathInfo, errRootPath
+		return rootPathInfo, errRootPath
 	}
 
 	return rootPathInfo, nil
@@ -35,38 +36,45 @@ func main() {
 	root := os.Getenv("HOME") + "/.reserve_emacs_configs"
 	// var rootNotExist bool = false
 	rootInfo, err := getRootInfo(root)
-		
-		if err != nil {
-			fmt.Println("Root does not exist. Attempting to create root")
-			err = mkroot(root)
-		}
 
-		if err != nil {
-			fmt.Println("Root does not exist, also failed to create root. Exiting")
-			fmt.Println(err)
-			return
-		}
+	if err != nil {
+		fmt.Println("Root does not exist. Attempting to create root")
+		err = mkroot(root)
+	}
 
-		rootInfo, err = getRootInfo(root)
+	if err != nil {
+		fmt.Println("Root does not exist, also failed to create root. Exiting")
+		fmt.Println(err)
+		return
+	}
 
-		if err != nil {
-			fmt.Println("Failed to create root again, exiting")
-			return
-		}
+	rootInfo, err = getRootInfo(root)
 
-    fmt.Println("Root information:")
-    fmt.Println(rootInfo)
+	if err != nil {
+		fmt.Println("Failed to create root again, exiting")
+		return
+	}
 
-		/*
-    err = filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-        files = append(files, path)
-        return nil
-    })
-    if err != nil {
-        panic(err)
-    }
-    for _, file := range files {
-        fmt.Println(file)
-		}
-		*/
+	fmt.Println("Root information:")
+	fmt.Println(rootInfo)
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s --current|--list|--set=[config]\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "--list provides currently available configs")
+		fmt.Fprintf(os.Stderr, "--current prints currently enabled config")
+		fmt.Fprintf(os.Stderr, "--set=[config] enables a desired config from the list")
+	}
+
+	/*
+		    err = filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		        files = append(files, path)
+		        return nil
+		    })
+		    if err != nil {
+		        panic(err)
+		    }
+		    for _, file := range files {
+		        fmt.Println(file)
+				}
+	*/
 }
